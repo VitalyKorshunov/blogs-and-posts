f256cd4e65f168b219facb0632247d0ec8257659
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-09-05 23:44:56 +0300

    updated few constant

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,7 @@
 import {config} from 'dotenv'
-
 config() // добавление переменных из файла .env в process.env
 
 export const SETTINGS = {
-    // все хардкодные значения должны быть здесь, для удобства их изменения
     PORT: process.env.PORT || 3003,
     PATH: {
         BLOGS: '/blogs',
@@ -14,8 +12,7 @@ export const SETTINGS = {
     ADMIN: process.env.ADMIN || '',
     MONGO_URL: process.env.MONGO_URL || '',
     DB_NAME: process.env.DB_NAME || '',
+    DB_NAME_FOR_TESTS: process.env.DB_NAME_FOR_TESTS || '',
     BLOG_COLLECTION_NAME: process.env.BLOG_COLLECTION_NAME || '',
     POST_COLLECTION_NAME: process.env.POST_COLLECTION_NAME || ''
 }
-
-// console.log(process.env.ADMIN)
