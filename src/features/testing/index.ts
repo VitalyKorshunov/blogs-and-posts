b86cf83d7b7ffdcd85a6f338f3524641f402c910
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-10-09 03:21:37 +0300

    test1 drop collection for vercel

--- src/features/testing/index.ts
+++ src/features/testing/index.ts
@@ -6,8 +6,6 @@ export const testingRouter = Router()
 
 testingRouter.delete(routersPaths.testing.allData, async (req, res) => {
     await blogCollection.drop()
-    await postCollection.drop()
-    await userCollection.drop()
-    await commentCollection.drop()
+
     res.status(204).json({})
 })
